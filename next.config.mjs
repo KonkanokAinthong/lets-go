63dc43259b11b43d79fc84ef5b9fda8959a8bd3d
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2024-05-06 22:30:03 +0700

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -18,5 +18,6 @@ export default withBundleAnalyzer({
   },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+    serverComponentsExternalPackages: ['puppeteer-core'],
   },
 });
